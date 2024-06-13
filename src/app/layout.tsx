3c1408869dd4508cceea16ad72b4f0a6b3552c19
Hammond ziggy <zorzorisrael6@gmail.com>
2024-06-13 19:56:16 +0100

    add ErrorBoundary

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,11 @@
 // src/app/layout.tsx
-// 'use client';
-
 import React from 'react';
 import './globals.css';
 import '../styles/fonts.css';
 import type { Metadata } from 'next';
 import { Analytics } from '@vercel/analytics/react';
 import { AuthProvider } from '@/utils/auth';
+import ErrorBoundary from './ErrorBoundary';
 
 export const metadata: Metadata = {
   title: 'Vendors Hub',
@@ -25,7 +24,9 @@ export default function RootLayout({
     <html lang="en">
       <body className='satoshi bg-primary text-black transition duration-300'>
         <AuthProvider>
-          {children}
+          <ErrorBoundary>
+            {children}
+          </ErrorBoundary>
         </AuthProvider>
         <Analytics />
       </body>
