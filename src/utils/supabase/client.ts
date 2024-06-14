8dd9155bb1219e65afcef744900958a6cfca82fe
Hammond ziggy <zorzorisrael6@gmail.com>
2024-06-14 07:24:02 +0100

    buid deployment fix01

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,7 +1,7 @@
 import { createBrowserClient } from '@supabase/ssr';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("Your project's URL and Key are required to create a Supabase client! Check your Supabase project's API settings to find these values.");
--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -3,7 +3,7 @@ import { cookies } from 'next/headers';
 
 // Fetch environment variables and ensure they are defined
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error(
