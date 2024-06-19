9e9bba180cce9ffc16813c8d36f7d62d097611e8
Hammond ziggy <zorzorisrael6@gmail.com>
2024-06-19 23:31:11 +0100

    bug-fix/fixing breaking changes

---
+++ src/app/SuspenseBoundary.tsx
@@ -0,0 +1,10 @@
+// SuspenseBoundary.tsx
+import React, { Suspense } from 'react';
+
+const SuspenseBoundary: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+  <Suspense fallback={<div>Loading...</div>}>
+    {children}
+  </Suspense>
+);
+
+export default SuspenseBoundary;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,24 +5,25 @@ import './globals.css';
 import '../styles/fonts.css';
 import { Analytics } from '@vercel/analytics/react';
 import { AuthProvider } from '@/utils/auth';
+import SuspenseBoundary from './SuspenseBoundary';
 import ErrorBoundary from './ErrorBoundary';
-import useNProgress from '@/components/useNProgress';
+// import useNProgress from '@/components/useNProgress';
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  useNProgress();
+  // useNProgress();
 
   return (
     <html lang="en">
       <body className='satoshi bg-primary text-black transition duration-300'>
         <AuthProvider>
           <ErrorBoundary>
-            <Suspense fallback={<div>Loading...</div>}>
+            <SuspenseBoundary>
               {children}
-            </Suspense>
+            </SuspenseBoundary>
           </ErrorBoundary>
         </AuthProvider>
         <Analytics />
