e2c22c2605d2137176437c6001f902ef83a5be6d
Hammond ziggy <zorzorisrael6@gmail.com>
2024-06-19 22:26:26 +0100

    still fixing errors

--- src/app/vendorsListings/page.tsx
+++ src/app/vendorsListings/page.tsx
@@ -1,19 +1,18 @@
-"use client";
-
-import { FC } from "react";
-import Categories from "@/components/common/categories";
+import { FC, Suspense } from 'react';
+import Categories from '@/components/common/categories';
 import Footer from '@/components/footer';
 import VendorsHero from './hero';
 
 const VendorsListings: FC = () => {
-
   return (
-    <div className="center flex-col w-full">
-      <VendorsHero />
-      <Categories />
-      <Footer />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="center flex-col w-full">
+        <VendorsHero />
+        <Categories />
+        <Footer />
+      </div>
+    </Suspense>
   );
-}
+};
 
 export default VendorsListings;
